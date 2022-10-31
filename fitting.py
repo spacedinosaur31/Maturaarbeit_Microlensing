458d478067fbe1bd353b638657e2b1eab593d9e7
spacedinosaur31 <103033746+spacedinosaur31@users.noreply.github.com>
2022-10-31 17:11:40 +0100

    Add files via upload

---
+++ fitting.py
@@ -0,0 +1,111 @@
+import numpy as np
+import os
+import time
+n_max_chi_value = 3 #because mean of standard deviation is approx. 1.5 
+#-> that's the noise of a constant source so same noise around ML theory function too
+
+# FUNCTIONS
+def f_ML_theo(n_d, n_umin, n_tE, n_I, n_t_max): #theoretische ML-Funktion, input = time-array, output = mag-array
+    #umin => between 0 and 1 - the smaller, the bigger the amplitude
+    #tE => duration of Event - the bigger, the wider the curve
+    #I => intensity I = (light intensity)/Area of star without amplification
+    #t_max => time when amplification A of I reaches maximum a
+    n_u = np.sqrt(n_umin**2 + ((n_d-n_t_max)/n_tE)**2)
+    if n_u*np.sqrt(n_u**2 + 4) != 0:
+        n_A = n_I*((n_u**2 + 2) / (n_u*np.sqrt(n_u**2 + 4))) 
+        n_M = -2.5*np.log10(n_A) # conversion luminosity to magnitude
+    else: n_M = -10 #if umin = 0 and d = t0, amplitude theoretically becomes infinite 
+    return n_M
+
+def f_ML_theo_for_array(a_t, n_umin, n_tE, n_I, n_t_max):
+    a_theo_mag = []
+    for n_t in a_t:
+        a_theo_mag.append(f_ML_theo(n_t, n_umin, n_tE, n_I, n_t_max))
+    return a_theo_mag
+
+
+def fit(f_ML_theo_for_array, a_x_t, a_y_mag): # returns number: optimal difference and array: (umin, tE, I, t_max)
+    a_differences = [] 
+    a_a_params = []
+    # Limits (numbers) - parameters beyond these are not sensible
+    
+    n_min_mag = min(a_y_mag)
+    n_max_mag = max(a_y_mag)
+    
+    n_min_t = int(min(a_x_t))
+    n_max_t = int(max(a_x_t))
+    
+    n_min_tE = 10
+    n_max_tE = n_max_t - n_min_t
+    n_steps_umin = 5  # range()-function takes ints only -> multiplies value do eliminate decimal stuff and divides back again
+    n_steps_mag = 5
+    start_fit = time.time()
+    for n_umin in [(1/n_steps_umin)*x for x in range(1,n_steps_umin)]: # umin has to be between 0 and 1 but range doesn't work for floats - make list of ints and divide again
+        for n_tE in range(n_min_tE, n_max_tE, 10):
+            for n_mag in [(1/n_steps_mag)*x for x in range(int(n_steps_mag*n_min_mag), int(n_steps_mag*n_max_mag))]: #for I-calculation, gets converted later
+                n_max_tmax = int(n_max_t - 0.5*n_tE)
+                n_min_tmax = int(n_min_t + 0.5*n_tE)
+                if n_min_tmax >= n_max_tmax: # manchmal kann tE zu gross werden für kleines Zeitintervall, sodass Fehler entstehen können
+                    continue
+                else:
+                    for n_t_max in range(n_min_tmax, n_max_tmax, 10): 
+                            a_theo_mag = f_ML_theo_for_array(
+                                a_x_t, 
+                                n_umin, 
+                                n_tE, 
+                                10**(n_mag/-2.5), # needs I as input but data in mag - conversion here
+                                n_t_max
+                            )
+                            chi_squared = np.sum(
+                                [
+                                    ((a_y_mag[x] - a_theo_mag[x])**2)/a_theo_mag
+                                    for x
+                                    in range(len(a_y_mag))
+                                ]
+                            )
+                            a_differences.append(chi_squared) # save in list and then calculate minimum 
+                            a_a_params.append([n_umin, n_tE, 10**(n_mag/-2.5), n_t_max])
+    stop_fit = time.time()
+    print(stop_fit-start_fit)
+    n_optimal_difference = min(a_differences)    
+    return [n_optimal_difference, a_a_params[a_differences.index(n_optimal_difference)]]
+        
+
+class O_indices: #object for indices as "objectid", not as "lc[0]" -> für die Übersichtlichkeit
+    n_objectid = 0
+    n_filterid = 1
+    n_fieldid = 2
+    n_rcid = 3
+    n_objra = 4
+    n_objdec = 5
+    n_nepochs = 6
+    a_hmjd = 7
+    a_mag = 8
+    a_magerr = 9
+    a_clrcoeff = 10
+    a_catflags = 11
+o_indices = O_indices()
+
+dir = "D:/ZTF_data/all_filtered_LCs" # hier Pfad zum Ordner, der alle Daten enthält 
+# FILTER 
+for s_path_root, a_s_folder, a_s_file in os.walk(dir): # für alle Ordner, Unterordner und Files des root-directorys
+    for s_name_file in a_s_file:
+        start = time.time()
+                # process the data
+        n_index = a_s_file.index(s_name_file)
+        print(f"-----------processing----------------")
+        print(f"file: {n_index}") # wissen, wo man steht
+        print("")
+        s_path_file = s_path_root + "/" + s_name_file # mache aufrufbaren Pfad (string), um auf File zuzugreifen
+        a_a_data = np.load(s_path_file, allow_pickle=True) # allow_pickle = True - error in new module version, needs this in order to funtion
+        
+        a_a_filtered = [ # array contains arrays of LC-array and minimal fitted difference-value-number
+            (a_LC, fit(f_ML_theo_for_array, a_LC[o_indices.a_hmjd], a_LC[o_indices.a_mag])) for a_LC in a_a_data # erstelle Liste aller LC, wenn Fitting-Programm bestanden
+            if 
+            fit(f_ML_theo_for_array, a_LC[o_indices.a_hmjd], a_LC[o_indices.a_mag])[0] < n_max_chi_value # erster Eintrag in Output Liste = Chi^2
+        ]
+        np.save(s_name_file + "_fitted", a_a_filtered) # File mit dem Namen s_name_file_fitted mit dem Inhalt a_a_filtered wird gespeichert
+        stop = time.time() 
+        print(stop-start) # wie lange das File brauchte
+         
+               
---
+++ make_json_url_list_jonas.js
@@ -0,0 +1,42 @@
+var s_url = "https://irsa.ipac.caltech.edu/data/ZTF/lc/lc_dr11/1/" // var creates a variable
+
+var a_o_url = Array.prototype.slice.call(document.querySelectorAll("a")) //gehe ins Webseitengerüst-Dokument und wähle alles mit "a" ("a"= hyperlink = Webseiten-Adresse)
+// obern: erstellt array gefüllt mit Links
+// Array.prototype.slice.call() does 
+
+var a_s_url_part = a_o_url.map(o=>o.href).filter(s=>s.includes("field")) //o und s sind Laufvariablen, .map ist wie for loop (inspiriert von funcitonal programming)
+//o.href = Attribut -> schlussendliche Adresse (Link kann anderen Namen im Vordergrund haben -> .href garantiert, dass es http://...ist)
+// falls "field" in o.href ist, tue o.href in a_s_url_part
+
+
+var a_s_url_parquet = []
+
+const parser = new DOMParser(); //DOMParser = Maschine im Hintergrund 
+//const = Deklarierung -> parser wird nicht überschrieben (Vermeidung von unberechenbaren Problemen)
+
+
+for(var s_url_part of a_s_url_part){
+
+    var o_response = await fetch(s_url_part); //await = asynchrones Data-Fetching -> warte, bis das darauffolgende geschafft ist, bevor du mit nächster Zeile weitermachst
+    // fetch(x) ruft x auf
+    var s_response = await o_response.text() // x.text() holt text aus x -> "xy"
+
+    let o_document = parser.parseFromString(s_response, "text/html");
+    // let ähnlich wie var
+    //hol mir die Seite in html-Format
+
+    var o_nodelist_a = o_document.querySelectorAll("a");
+    // alle mit tag "a"
+    
+    console.log("length")
+    console.log(o_nodelist_a.length)
+    //console.log(x) = print(x)
+
+    a_s_url_parquet = a_s_url_parquet.concat(Array.prototype.slice.call(o_nodelist_a).map(o=>s_url_part+o.href.split("/").pop()).filter(s=>s.includes(".parquet")))
+    // setzt endgültigen Link zusammen
+    //.split("/").pop() -> lösche bis Schrägstrich und hänge absoluten Pfad vorne an mit field (sodass man nicht immer durch alle Ordner muss sodnern direkt)
+    console.log(a_s_url_parquet)
+    // console.log(o_document.querySelectorAll("a")[5].href)
+}
+JSON.stringify(a_s_url_parquet) // macht .json-file = -> {"data":"data"}
+//
